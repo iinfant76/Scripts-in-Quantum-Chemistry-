a3d61d8fc1d6b7f9e4b1c0625b01de58b8db4098
Ivan Infante <iinfant76@gmail.com>
2018-05-21 21:18:41 +0200

    added new functions for cleaning up code

--- general/md.py
+++ general/md.py
@@ -1,4 +1,5 @@
-__all__ = ['make_bond_matrix', 'rdf']
+__all__ = ['make_bond_matrix', 'get_numberofatoms', 'read_atomlist',
+           'read_xyz_coordinates', 'rdf']
 
 import numpy as np 
 import subprocess
@@ -19,7 +20,7 @@ def make_bond_matrix(n_atoms, coords):
             )
     return r
 
-def rdf(fn, atoms_i, atoms_j, dr, rmax, start, stride): 
+def get_numberofatoms(fn): 
     # Retrieve number of lines in trajectory file
     cmd = "wc -l {}".format(fn)
     l = subprocess.check_output(cmd.split()).decode()
@@ -29,6 +30,26 @@ def rdf(fn, atoms_i, atoms_j, dr, rmax, start, stride):
     with open(fn) as f:
        l = f.readline()
        n_atoms = int(l.split()[0])
+    
+    return n_atoms 
+
+def read_atomlist(fn, n_atoms):
+    # Read atomic list from xyz file
+    atoms = pd.read_csv(fn, nrows = n_atoms, delim_whitespace=True, header=None, 
+                        skiprows = 2, usecols=[0]).astype(str).values
+    return atoms     
+
+
+def read_xyz_coordinates(fn, n_atoms, iframe): 
+    # Read xyz coordinate from a (trajectory) xyz file. 
+    coords = pd.read_csv(fn, nrows = n_atoms, delim_whitespace=True, header=None, 
+             skiprows = (2 + (n_atoms + 2) * (iframe - 1)), usecols=(1,2,3)).astype(float).values
+    
+    return coords  
+
+def rdf(fn, atoms_i, atoms_j, dr, rmax, start, stride): 
+    # Get the number of atoms from the trajectory file
+    n_atoms = get_numberofatoms(fn) 
 
     # Calculate total number of frames in the trajectory file  
     n_frames = int(int(n_lines)/(n_atoms+2))
@@ -41,21 +62,16 @@ def rdf(fn, atoms_i, atoms_j, dr, rmax, start, stride):
     vol_tot = np.sum(volume) 
     
     # Read the order of atoms from the first frame of the trajectory. This order is unchanged. 
-#    atoms = np.genfromtxt(fn, skip_header = 2, skip_footer=(int(n_lines) - (n_atoms + 2)), usecols=0, dtype=str)
-    atoms = pd.read_csv(fn, nrows = n_atoms, delim_whitespace=True, header=None, 
-                        skiprows = 2, usecols=[0]).astype(str).values
+    atoms = read_atomlist(fn, n_atoms) 
+
     # Find the indexes in the bond_matrix of the atomic types involved in the calculation of g_ij
     index_i = np.where( atoms == atoms_i )
     index_j = np.where( atoms == atoms_j )
 
     for iframe in range(start, n_frames, stride): 
         # Read coordinates from iframe
-#        coords = np.genfromtxt(fn, skip_header = (2 + (n_atoms + 2) * (iframe - 1)), 
-#                    skip_footer=(int(n_lines) - ((n_atoms + 2) * iframe)), usecols=(1,2,3))
-        coords = pd.read_csv(fn, nrows = n_atoms, delim_whitespace=True, header=None, 
-                    skiprows = (2 + (n_atoms + 2) * (iframe - 1)), usecols=(1,2,3)).astype(float).values
+        coords = read_xyz_coordinates(fn, n_atoms, iframe) 
         # Compute bond distance matrix for iframe
-       # bond_mtx = make_bond_matrix(n_atoms, coords)
         bond_mtx = make_bond_matrix_f(coords, n_atoms)
         # Slice the bond_matrix with only the atom types
         sliced_mtx = np.triu(bond_mtx[np.ix_(index_i[0], index_j[0])]) 
@@ -81,14 +97,7 @@ def rdf(fn, atoms_i, atoms_j, dr, rmax, start, stride):
 
 def adf(fn, atoms_i, atoms_j, atoms_k, start, stride): 
     # Retrieve number of lines in trajectory file
-    cmd = "wc -l {}".format(fn)
-    l = subprocess.check_output(cmd.split()).decode()
-    n_lines = int(l.split()[0]) # Number of lines in traj file
-
-    # Read number of atoms in the molecule. It is usually the first line in a xyz file  
-    with open(fn) as f:
-       l = f.readline()
-       n_atoms = int(l.split()[0])
+    n_atoms = get_numberofatoms(fn) 
 
     # Calculate total number of frames in the trajectory file  
     n_frames = int(int(n_lines)/(n_atoms+2))
@@ -96,14 +105,9 @@ def adf(fn, atoms_i, atoms_j, atoms_k, start, stride):
     # Create grid of r values to compute the pair correlation function g_ij 
     ang_grid = np.arange(0, 360, 1)
     
-    # Compute the volume in a concentric sphere of size dr at a distance r from the origin  
-#    volume = 4 * np.pi * np.power(r_grid, 2) * dr # elemental volume dV = 4*pi*r^2*dr
-#    vol_tot = np.sum(volume) 
-    
     # Read the order of atoms from the first frame of the trajectory. This order is unchanged. 
-#    atoms = np.genfromtxt(fn, skip_header = 2, skip_footer=(int(n_lines) - (n_atoms + 2)), usecols=0, dtype=str)
-    atoms = pd.read_csv(fn, nrows = n_atoms, delim_whitespace=True, header=None, 
-                        skiprows = 2, usecols=[0]).astype(str).values
+    atoms = read_atomlist(fn, n_atoms) 
+
     # Find the indexes in the bond_matrix of the atomic types involved in the calculation of g_ij
     index_i = np.where( atoms == atoms_i )
     index_j = np.where( atoms == atoms_j )
@@ -111,12 +115,10 @@ def adf(fn, atoms_i, atoms_j, atoms_k, start, stride):
     
     for iframe in range(start, n_frames, stride): 
         # Read coordinates from iframe
-#        coords = np.genfromtxt(fn, skip_header = (2 + (n_atoms + 2) * (iframe - 1)), 
-#                    skip_footer=(int(n_lines) - ((n_atoms + 2) * iframe)), usecols=(1,2,3))
-        coords = pd.read_csv(fn, nrows = n_atoms, delim_whitespace=True, header=None, 
-                    skiprows = (2 + (n_atoms + 2) * (iframe - 1)), usecols=(1,2,3)).astype(float).values
+        coords = read_xyz_coordinates(fn, n_atoms, iframe) 
         # Compute bond distance matrix for iframe
         bond_mtx = make_bond_matrix_f(coords, n_atoms)
+        # Compute the angle matrix and convert it in degrees 
         teta_mtx = np.degrees(make_angle_matrix_f(bond_mtx, coords, n_atoms))
         # Slice the bond_matrix with only the atom types
         sliced_mtx = np.triu(teta_mtx[np.ix_(index_i[0], index_j[0], index_k[0])]) 
@@ -125,11 +127,8 @@ def adf(fn, atoms_i, atoms_j, atoms_k, start, stride):
                     np.where( (sliced_mtx > ang_grid[idr]) & (sliced_mtx < ang_grid[idr+1]) )[0].size 
                     for idr in range(ang_grid.size-1))
         # Compute the total density of pair of atoms
-        #rho_tot = np.sum(rho_ab) 
-        #rho_bulk = rho_tot / vol_tot  
-#        density = counts / volume[1:] # skip the first element dV which is 0 
-        g_abc = rho_ab #/ rho_bulk 
-        # Store density in g_ij
+        g_abc = rho_ab 
+        # Store density in g_ijk
         if (iframe == start):
             g_ijk = g_abc
         else:
