5341a251a684e644acc85ab9a3f66f001a845b71
Ivan Infante <iinfant76@gmail.com>
2018-05-21 21:19:35 +0200

    changed init file to include new functions

--- general/__init__.py
+++ general/__init__.py
@@ -6,5 +6,6 @@ from .md import (make_bond_matrix, rdf)
 
 __all__ = ['atomic_number', 'atomic_mass',
            'make_bond_matrix_f', 'make_angle_matrix_f',
-           'make_bond_matrix', 'rdf'] 
+           'make_bond_matrix', 'rdf',
+           'get_numberofatoms', 'read_atomlist', 'read_xyz_coordinates'] 
 
